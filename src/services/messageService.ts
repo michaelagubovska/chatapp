8d290ca196b66656e6c22c686ac0444779d5afb5
Michaela Gubovska <michaela.gubovska@gmail.com>
2023-10-08 09:28:30 +0200

    remove unused subject method

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -4,6 +4,5 @@ const subject = new Subject();
 
 export const messageService = {
     sendMessage: ( message ) => subject.next(message),
-    clearMessages: () => subject.next({}),
     getMessage: () => subject.asObservable()
 };
